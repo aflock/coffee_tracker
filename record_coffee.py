0861bc251b240422bf14aa87d33f9ba85d768149
Andrew Flockhart <aflock@fb.com>
2015-10-24 00:18:22 -0400

    record_coffee

--- record_coffee.py
+++ record_coffee.py
@@ -1,7 +1,24 @@
-def main():
+import csv
+import os
+import time
+# Constants
+FILENAME = "coffeelog.csv"
+FIELDS = ['mg', 'date', 'format']
+DEFAULT_MG = 80
+DEFAULT_FMT = 'coffee'
 
-def record_coffee():
-    return
+def main():
+    initial_run = FILENAME not in os.listdir('.')
+    record_coffee(initial_run)
+
+
+def record_coffee(initial_run=False):
+    with open(FILENAME, 'a') as f:
+        writer = csv.DictWriter(f, fieldnames = FIELDS)
+        if initial_run:
+            writer.writeheader()
+        writer.writerow({'mg': DEFAULT_MG, 'format': DEFAULT_FMT, 'date': time.time()})
+
 def upload():
     return
 
